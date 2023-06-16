8b37b89f8a1378d2d665fdbc2aefbfff6acb4f91
Vivek Vaish <vaishvivek634@gmail.com>
2023-06-16 16:51:30 +0530

    re-commenting the code

--- file_maker.py
+++ file_maker.py
@@ -1,12 +1,30 @@
 import pickle
 
+# in case, if the other files are not working properly. Then you have to delete the old file and create the new file from here
+
+# steps to create a new file:-
+# 1. select which file you want to create
+# 2. remove the comments from below code to create the new file
+# 3. run the program again
+
+
+
+# for "font.dat" file
+
 # with open('font.dat', 'wb') as f:
 #     pickle.dump('Georgia', f)
 #     print('File Created!')
 
-with open('recent_file.dat', 'wb') as f:
-    pickle.dump('', f)
-    print('File Created!')
+
+
+# for "recent_file.dat" file
+
+# with open('recent_file.dat', 'wb') as f:
+#     pickle.dump('', f)
+#     print('File Created!')
+
+
+# for "size.dat" file
 
 # with open('size.dat', 'wb') as f:
 #     pickle.dump('20', f)
